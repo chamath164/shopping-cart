ba447f59031e55347335d9498266dbf35eea0fd9
chamath7 <chamalakshan164@gmail.com>
2021-02-04 12:18:03 +0530

    Add services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './component/shared/nav/nav.component';
+import { HeaderComponent } from './component/shared/header/header.component';
+import { FooterComponent } from './component/shared/footer/footer.component';
+import { ShoppingCartComponent } from './component/shopping-cart/shopping-cart.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FiltersComponent } from './component/shopping-cart/filters/filters.component';
+import { ProductListComponent } from './component/shopping-cart/product-list/product-list.component';
+import { CartComponent } from './component/shopping-cart/cart/cart.component';
+import { CartItemComponent } from './component/shopping-cart/cart/cart-item/cart-item.component';
+import { ProductItemComponent } from './component/shopping-cart/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent,
+    ShoppingCartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    CartComponent,
+    CartItemComponent,
+    ProductItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
