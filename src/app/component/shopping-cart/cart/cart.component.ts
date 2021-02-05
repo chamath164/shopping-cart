2428bcd4d2567945692a8b046686b4c52b9dc9cf
chamath7 <chamalakshan164@gmail.com>
2021-02-05 12:00:20 +0530

    AddToCart Button

--- src/app/component/shopping-cart/cart/cart.component.ts
+++ src/app/component/shopping-cart/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MessengerService } from '../../../services/messenger.service';
+import {Product} from '../../../models/product';
 
 @Component({
   selector: 'app-cart',
@@ -7,21 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
   cartItems = [
-    { id: 1, productId: 1, productName: 'test1', qty: 2, price: 100},
-    { id: 2, productId: 2, productName: 'test2', qty: 3, price: 200},
-    { id: 3, productId: 3, productName: 'test3', qty: 4, price: 300},
-    { id: 4, productId: 4, productName: 'test4', qty: 5, price: 400},
-    { id: 5, productId: 5, productName: 'test5', qty: 6, price: 500}
+    // { id: 1, productId: 1, productName: 'test1', qty: 2, price: 100},
+    // { id: 2, productId: 2, productName: 'test2', qty: 3, price: 200},
+    // { id: 3, productId: 3, productName: 'test3', qty: 4, price: 300},
+    // { id: 4, productId: 4, productName: 'test4', qty: 5, price: 400},
+    // { id: 5, productId: 5, productName: 'test5', qty: 6, price: 500},
   ];
 
   cartTotal = 0;
 
-  constructor() { }
+  constructor(private msg: MessengerService) { }
 
   ngOnInit(): void {
-    this.cartItems.forEach(item => {
-      this.cartTotal += (item.price * item.qty);
+    this.msg.getMsg().subscribe((product: Product) => {
+      this.addProductToCart(product);
     });
   }
+  // tslint:disable-next-line:typedef
+  addProductToCart(product: Product){
 
+    let productExists = false;
+    for(let i in this.cartItems) {
+      if (this.cartItems[i].productId === product.id) {
+        this.cartItems[i].qty++;
+        productExists = true;
+        break;
+      }
+    }
+
+    if(!productExists){
+      this.cartItems.push({
+            productId: product.id,
+            productName: product.name,
+            qty: 1,
+            price: product.price
+      });
+    }
+
+    this.cartTotal = 0;
+    this.cartItems.forEach(item => {
+        this.cartTotal += (item.price * item.qty)
+      });
+    }
 }
--- src/app/component/shopping-cart/product-list/product-item/product-item.component.ts
+++ src/app/component/shopping-cart/product-list/product-item/product-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { MessengerService } from '../../../../services/messenger.service';
 
 @Component({
   selector: 'app-product-item',
@@ -9,9 +10,11 @@ import { Product } from 'src/app/models/product';
 export class ProductItemComponent implements OnInit {
 
   @Input() productItem: Product;
-  constructor() { }
+  constructor(private msg: MessengerService) { }
 
   ngOnInit(): void {
   }
-
+  handleAddToCarat(){
+    this.msg.sendMsg(this.productItem);
+  }
 }
